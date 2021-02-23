0850505f8972a76b08ecf55c4bab9e93d3ff2049
SylphidZoul <zoulxhikaru@gmail.com>
2021-02-23 20:19:29 -0300

    Added Posts' Service

---
+++ src/services/posts.js
@@ -0,0 +1,51 @@
+const PostStore = require('../store/posts')
+
+class PostsService {
+  constructor () {
+    this.store = new PostStore()
+  }
+
+  async getAllPosts () {
+    const attributes = ['id', 'title', 'image', 'creationDate']
+    const order = [['creationDate', 'desc'], ['id', 'desc']]
+    const posts = await this.store.findAll(attributes, order)
+
+    return posts
+  }
+
+  async getPost (id) {
+    const where = { id }
+    const post = await this.store.findOne(where)
+
+    if (!post) throw new Error(`The post with id "${id}" doesn't exists.`)
+
+    return post
+  }
+
+  async createPost (newPostData) {
+    const newPost = { ...newPostData, creationDate: new Date() }
+    const createdPost = await this.store.create(newPost)
+
+    return { createdPost }
+  }
+
+  async updatePost (id, updateData) {
+    const where = { id }
+    const wasUpdated = await this.store.update(where, updateData)
+    if (!wasUpdated) throw new Error('The post could not be updated.')
+
+    const updatedPost = await this.store.findOne(where)
+
+    return { updatedPost }
+  }
+
+  async deletePost (id) {
+    const where = { id }
+    const wasDeleted = await this.store.delete(where)
+    if (!wasDeleted) throw new Error('The post could not be deleted.')
+
+    return { deletedPost: id }
+  }
+}
+
+module.exports = PostsService
